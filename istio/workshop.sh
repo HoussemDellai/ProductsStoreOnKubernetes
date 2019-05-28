954b75ff7345fe050133d140c488fca2e9c27a69
git <houssem.dellai@live.com>
2019-05-28 13:43:07 +0200

    added istio workshop

--- istio/workshop.sh
+++ istio/workshop.sh
@@ -1,3 +1,9 @@
+# Install Istion on AKS
+# https://docs.microsoft.com/en-us/azure/aks/istio-install
+
+# This workshop was highly inspired by:
+# https://docs.microsoft.com/en-us/azure/aks/istio-scenario-routing
+
 kubectl create namespace products
 
 kubectl label namespace products istio-injection=enabled
@@ -6,20 +12,28 @@ kubectl get ns voting -o yaml
 # apiVersion: v1
 # kind: Namespace
 # metadata:
-#   creationTimestamp: "2019-05-27T14:17:59Z"
 #   labels:
-#     istio-injection: enabled
-#   name: products
-#   resourceVersion: "7881"
-#   selfLink: /api/v1/namespaces/products
-#   uid: 3b235de6-808a-11e9-9ec6-0a58ac1f1263
-# spec:
-#   finalizers:
-#   - kubernetes
-# status:
-#   phase: Active
+#     istio-injection: enabled    # will be considered by Istio
+# ...
 
-kubectl apply -n products -f mssql-secret.yaml
-kubectl apply -n products -f mssql-pv.azure.yaml
-kubectl apply -n products -f mssql-deployment.yaml
-kubectl apply -n products -f mvc-deployment.azure.yaml
+# deploy the v1.0 of the application
+kubectl apply -n products -f mvc-app-v1.0.yaml
+kubectl describe pod mvc-app-1-0-565fc694d8-q9rvr --namespace products
+# ...
+# Containers:
+#   mvc-app:
+#     Image:         houssemdocker/products-store-mvc:v1.0
+#     ...
+#   istio-proxy:
+#     Image:         docker.io/istio/proxyv2:1.1.3
+# ...
+
+# deploy a Gateway to access the app
+kubectl apply -n products -f mvc-app-gateway.yaml
+
+# get the public IP address to access the app
+kubectl get service istio-ingressgateway --namespace istio-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+
+# deploy v1.1 and v2.0
+kubectl apply -n products -f mvc-deployment.v1.1.yaml
+kubectl apply -n products -f mvc-deployment.v2.0.yaml
