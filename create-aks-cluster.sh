0968bd0d8f4f01353daa8436d3e21d5d53f71cef
git <houssem.dellai@live.com>
2019-06-06 15:19:13 +0200

    edited docs

---
+++ create-aks-cluster.sh
@@ -0,0 +1,37 @@
+# connect to Azure via Azure CLI
+az login
+
+# create variables
+# AksName="aks-k8s-extia"
+# AksRG="aks-k8s-extia"
+
+# Create resource group
+az group create \
+   --location westeurope \ 
+   --subscription "Microsoft Azure Sponsorship" \
+   --name aks-k8s-rg 
+
+# create AKS cluster
+az aks create \
+   --generate-ssh-keys \
+   --subscription "Microsoft Azure Sponsorship" \
+   --node-count 3 \
+   --resource-group aks-k8s-rg \
+   --name aks-k8s
+
+# connect to cluster
+az aks get-credentials \
+   --resource-group aks-k8s-rg \
+   --name aks-k8s \
+   --subscription "Microsoft Azure Sponsorship"
+
+# get access to Dashboard
+kubectl create clusterrolebinding kubernetes-dashboard \
+        --clusterrole=cluster-admin \
+        --serviceaccount=kube-system:kubernetes-dashboard
+
+# Open Dashboard
+az aks browse \
+   --resource-group aks-k8s-rg \
+   --name aks-k8s \
+   --subscription "Microsoft Azure Sponsorship" 
