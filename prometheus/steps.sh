c2463a589857440484bfd36c4bcb22d2774be7ff
Houssem Dellai <houssem.dellai@live.com>
2019-10-07 09:31:40 +0200

    updated steps.sh

--- prometheus/steps.sh
+++ prometheus/steps.sh
@@ -1,16 +1,3 @@
-// docs: https://linuxacademy.com/blog/kubernetes/running-prometheus-on-kubernetes/
-
-kubectl apply -f namespace.yaml
-kubectl apply -f clusterRole.yaml
-kubectl apply -f configmap.yaml
-kubectl apply -f prometheus-deployment.yaml
-
-kubectl get pods -n monitoring
-kubectl get services -n monitoring
-
-
-#########################################
-
 # Install Prometheus using helm Charts
 $ helm install stable/prometheus --name my-prometheus --set server.service.type=LoadBalancer --set rbac.create=false
 
