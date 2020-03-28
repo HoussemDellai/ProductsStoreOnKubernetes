69ed8570f59742b3f317cd59ecc1620e313f3b9d
Houssem Dellai <hodellai@microsoft.com>
2020-03-28 14:13:52 +0100

    edited actions

--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,10 @@ provider "azurerm" {
   version         = ">=2.0"
   # Get the following credentials by running the command:
   # az ad sp create-for-rbac --sdk-auth
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
   # The "feature" block is required for AzureRM provider 2.x.
   features {}
 }
