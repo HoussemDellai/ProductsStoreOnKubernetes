07da47ed776c119b157da620a0ac26fd4a819e41
Houssem Dellai <houssem.dellai@live.com>
2020-05-09 17:27:30 +0200

    enabled Managed Identity in AKS TF template

--- infra/main.tf
+++ infra/main.tf
@@ -28,13 +28,16 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     vm_size    = "Standard_DS2_v2"
   }
 
+  identity {
+    type = "SystemAssigned"
+  }
   # service_principal {
   #   client_id     = var.client_id
   #   client_secret = var.client_secret
   # }
 
   role_based_access_control {
-    enabled = false
+    enabled = true
   }
 
   network_profile {
