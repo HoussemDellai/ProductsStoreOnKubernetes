3c6e608ce8be73f9577002d0a5bda1dcd9917930
Houssem Dellai <houssem.dellai@live.com>
2020-05-09 18:44:09 +0200

    added aks_identity as output

--- infra/output.tf
+++ infra/output.tf
@@ -25,3 +25,7 @@ output "kube_config" {
 output "host" {
   value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
 }
+
+output "aks_identity" {
+  value = azurerm_kubernetes_cluster.k8s.identity
+}
