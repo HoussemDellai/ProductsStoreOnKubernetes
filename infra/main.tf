3199e0aae675e7f77a958bd7fc3feebd60f2aed0
Houssem Dellai <houssem.dellai@live.com>
2020-05-10 11:24:56 +0200

    replaced #{var}# by __var__

--- infra/main.tf
+++ infra/main.tf
@@ -51,11 +51,11 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = var.acr_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  sku                      = "Standard"
-  admin_enabled            = false
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = false
 }
 
 resource "azurerm_sql_server" "sql" {
