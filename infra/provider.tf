7fcf91f71d0eba16a1f8552c49e62a76e6513b93
Houssem Dellai <houssem.dellai@live.com>
2020-12-12 10:31:07 +0100

    added WebApp project and upgraded MvcApp to .NET 5.0

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   features {}
   # Get the following credentials by running the command:
-  # $ az ad sp create-for-rbac --sdk-auth
+  # $ az ad sp create-for-rbac --role owner --sdk-auth
   # subscription_id = var.subscription_id
   # client_id       = var.client_id
   # client_secret   = var.client_secret
