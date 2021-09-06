e3d98701cd76270c8b148ed71a17d1d03812e47a
Houssem Dellai <houssem.dellai@live.com>
2021-09-06 13:32:47 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
-  admin_enabled       = false
+  admin_enabled       = true # false
 }
 
 resource "azurerm_sql_server" "sql" {
