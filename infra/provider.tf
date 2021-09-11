12ceea71ad5ef19b5215db6360e51b2262c651f5
Houssem Dellai <houssem.dellai@live.com>
2021-09-11 12:43:15 +0200

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,11 +1,12 @@
 provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
   features {}
-  # Get the following credentials by running the command:
-  # $ az ad sp create-for-rbac --role owner --sdk-auth
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  # tenant_id       = var.tenant_id
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.76.0"
+    }
+  }
 }
