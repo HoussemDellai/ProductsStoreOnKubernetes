54da0872c2bf51339e54cd9bcfdbcbf4ab3ce049
Houssem Dellai <houssem.dellai@live.com>
2022-04-23 14:32:38 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -24,10 +24,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   #   client_secret = var.client_secret
   # }
 
-  role_based_access_control {
-    enabled = true
-  }
-
   network_profile {
     network_plugin = "kubenet"
     network_policy = "calico"
